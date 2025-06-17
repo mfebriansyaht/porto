274c24bbe1d1c39c0e4d2294828639cb2e5a961a
Moch Febriansyah Trisnadi <febriansyah.trisnadi@bankmega.com>
2025-06-17 09:32:41 +0700

    update

--- components/intro.tsx
+++ components/intro.tsx
@@ -113,14 +113,6 @@ export default function Intro() {
         >
           <BsWhatsapp />
         </a>
-
-        <a
-          className="bg-white p-4 text-gray-700 flex items-center gap-2 text-[1.35rem] rounded-full focus:scale-[1.15] hover:scale-[1.15] hover:text-gray-950 active:scale-105 transition cursor-pointer borderBlack dark:bg-white/10 dark:text-white/60"
-          href="https://github.com"
-          target="_blank"
-        >
-          <FaGithubSquare />
-        </a>
       </motion.div>
     </section>
   );
